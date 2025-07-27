8f4ed9e6068a8847f87bb740e09a59fc23ab3d37
Tim Swast <tswast@gmail.com>
2025-07-27 18:26:51 -0500

    complete functions exercises

--- exercises/02_functions/functions1.rs
+++ exercises/02_functions/functions1.rs
@@ -1,4 +1,8 @@
 // TODO: Add some function with the name `call_me` without arguments or a return value.
+fn call_me() {
+    println!("Hello functions!");
+}
+
 
 fn main() {
     call_me(); // Don't change this line
