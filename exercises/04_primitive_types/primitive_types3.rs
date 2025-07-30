4dd19601942f387ab80459970cc2c347f0d995e1
Tim Swast <tswast@gmail.com>
2025-07-29 19:45:49 -0500

    finish primitive types exercises

--- exercises/04_primitive_types/primitive_types3.rs
+++ exercises/04_primitive_types/primitive_types3.rs
@@ -1,9 +1,12 @@
 fn main() {
     // TODO: Create an array called `a` with at least 100 elements in it.
-    // let a = ???
+    let mut a : [i32; 1000] = [0; 1000];
+    for (i, item) in a.iter_mut().enumerate() {
+        *item = i32::try_from(i).expect("shouldnt happen");
+    }
 
     if a.len() >= 100 {
-        println!("Wow, that's a big array!");
+        println!("Wow, that's a big array! {:?}", a);
     } else {
         println!("Meh, I eat arrays like that for breakfast.");
         panic!("Array not big enough, more elements needed");
