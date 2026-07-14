b4e712b4477f82c9649f95bbe1edc861110aa811
Tim Swena <swast@google.com>
2026-07-14 14:31:15 -0500

    complete vecs and move_semantics exercise

--- exercises/06_move_semantics/move_semantics2.rs
+++ exercises/06_move_semantics/move_semantics2.rs
@@ -20,7 +20,9 @@ mod tests {
     fn move_semantics2() {
         let vec0 = vec![22, 44, 66];
 
-        let vec1 = fill_vec(vec0);
+        let mut vec1 = Vec::<i32>::with_capacity(vec0.len() + 1);
+        vec0.clone_into(&mut vec1);
+        vec1 = fill_vec(vec1);
 
         assert_eq!(vec0, [22, 44, 66]);
         assert_eq!(vec1, [22, 44, 66, 88]);
